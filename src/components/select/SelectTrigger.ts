cb392ab7dd1bdc4340328027183b81564850bd8a
Nemanja Malesija <malesija.nemanja@gmail.com>
2026-02-05 00:28:43 +0100

    feat(select): add control, trigger, clear, and empty components

---
+++ src/components/select/SelectTrigger.ts
@@ -0,0 +1,39 @@
+import { defineComponent, h, type Component, type PropType } from 'vue'
+import { Primitive } from '../Primitive'
+import { useSelectContext } from './selectContext'
+import { mergeProps } from '../../utils/mergeProps'
+
+export const SelectTrigger = defineComponent({
+  name: 'SelectTrigger',
+  inheritAttrs: false,
+  props: {
+    as: {
+      type: [String, Object] as PropType<string | Component>,
+      default: 'button',
+    },
+  },
+  setup(props, { attrs, slots }) {
+    const ctx = useSelectContext<unknown>()
+
+    return () => {
+      const triggerProps = mergeProps(
+        {
+          type: 'button',
+          'aria-expanded': ctx.isOpen.value,
+          'aria-controls': ctx.getListboxProps().id,
+          onClick: () => ctx.toggle(),
+        },
+        attrs,
+      )
+
+      return h(
+        Primitive,
+        {
+          as: props.as,
+          ...triggerProps,
+        },
+        slots.default?.(),
+      )
+    }
+  },
+})
