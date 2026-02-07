e19f7e32ea63f5d6f0d9ac3b72ae196f43d0d079
Nemanja Malesija <malesija.nemanja@gmail.com>
2026-02-07 12:44:37 +0100

    style(format): enforce 2-space editor indentation

--- src/components/select/SelectRoot.ts
+++ src/components/select/SelectRoot.ts
@@ -4,7 +4,7 @@ import type { FilterFn } from '../../composables/useFilter'
 import { provideSelectContext } from './selectContext'
 
 export const SelectRoot = defineComponent({
-    name: 'SelectRoot',
+  name: 'SelectRoot',
   props: {
     modelValue: {
       type: null as unknown as PropType<unknown | null>,
@@ -14,49 +14,49 @@ export const SelectRoot = defineComponent({
       type: null as unknown as PropType<unknown | null>,
       default: null,
     },
-        open: {
-            type: Boolean as PropType<boolean | undefined>,
-            default: undefined,
-        },
-        defaultOpen: {
-            type: Boolean,
-            default: false,
-        },
-        filter: {
-            type: Function as PropType<FilterFn<unknown>>,
-            default: undefined,
-        },
-        debounce: {
-            type: Number,
-            default: undefined,
-        },
-        loop: {
-            type: Boolean,
-            default: true,
-        },
-        id: {
-            type: String,
-            default: undefined,
-        },
+    open: {
+      type: Boolean as PropType<boolean | undefined>,
+      default: undefined,
     },
-    emits: ['update:modelValue', 'update:open'],
-    setup(props, { emit, slots, expose }) {
-        const api = useSelect({
-            id: props.id,
-            value: toRef(props, 'modelValue'),
-            defaultValue: props.defaultValue,
-            onValueChange: (value) => emit('update:modelValue', value),
-            open: toRef(props, 'open'),
-            defaultOpen: props.defaultOpen,
-            onOpenChange: (open) => emit('update:open', open),
-            filter: props.filter,
-            debounce: props.debounce,
-            loop: props.loop,
-        })
-
-        provideSelectContext(api)
-        expose({ open: api.open, close: api.close, toggle: api.toggle })
-
-        return () => slots.default?.()
+    defaultOpen: {
+      type: Boolean,
+      default: false,
     },
+    filter: {
+      type: Function as PropType<FilterFn<unknown>>,
+      default: undefined,
+    },
+    debounce: {
+      type: Number,
+      default: undefined,
+    },
+    loop: {
+      type: Boolean,
+      default: true,
+    },
+    id: {
+      type: String,
+      default: undefined,
+    },
+  },
+  emits: ['update:modelValue', 'update:open'],
+  setup(props, { emit, slots, expose }) {
+    const api = useSelect({
+      id: props.id,
+      value: toRef(props, 'modelValue'),
+      defaultValue: props.defaultValue,
+      onValueChange: (value) => emit('update:modelValue', value),
+      open: toRef(props, 'open'),
+      defaultOpen: props.defaultOpen,
+      onOpenChange: (open) => emit('update:open', open),
+      filter: props.filter,
+      debounce: props.debounce,
+      loop: props.loop,
+    })
+
+    provideSelectContext(api)
+    expose({ open: api.open, close: api.close, toggle: api.toggle })
+
+    return () => slots.default?.()
+  },
 })
